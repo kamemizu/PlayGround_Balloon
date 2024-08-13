254318d0ff6b52678b2ae9119b3a53f4dffaf1ff
kamemizu <kamemizuga@gmail.com>
2024-08-14 07:44:24 +0900

    シーン遷移実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -152,5 +153,10 @@ public class GameManager : MonoBehaviour
             bestTime = nowTime;
         }
         bestTimeText.GetComponent<Text>().text = String.Format("‚³‚¢‚»‚­‚«‚ë‚­\n" + "{0:####.0}" + "‚Ñ‚å‚¤", bestTime);
+    }
+
+    public void PushTitle()
+    {
+        SceneManager.LoadScene("Title");
     }
 }
